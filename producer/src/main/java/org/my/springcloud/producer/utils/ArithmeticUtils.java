3743677fa635e2b0e62ebb875764e633f10003e9
liulang <liulang@egova.com.cn>
2023-02-13 20:17:02 +0800

    [M][0]二叉树算法

--- producer/src/main/java/org/my/springcloud/producer/utils/ArithmeticUtils.java
+++ producer/src/main/java/org/my/springcloud/producer/utils/ArithmeticUtils.java
@@ -601,4 +601,46 @@ public class ArithmeticUtils {
 
         return ret;
     }
+
+    /**
+     * 返回树的深度
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    public int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            // 记录层数
+            ans++;
+        }
+        return ans;
+    }
+
+
 }
