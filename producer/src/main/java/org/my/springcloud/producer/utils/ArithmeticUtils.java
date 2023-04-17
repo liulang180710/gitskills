bcddab628b7f5e7098765087ada4f6f8857f9843
liulang <liulang@egova.com.cn>
2023-04-17 11:21:12 +0800

    [M][0]回文字符串判断

--- producer/src/main/java/org/my/springcloud/producer/utils/ArithmeticUtils.java
+++ producer/src/main/java/org/my/springcloud/producer/utils/ArithmeticUtils.java
@@ -811,4 +811,90 @@ public class ArithmeticUtils {
         return dfs(left.left,right.right) && dfs(left.right,right.left);
     }
 
+    /**
+     * 根据前序遍历和中序遍历重构树
+     */
+    private Map<Integer, Integer> indexMap;
+    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) {
+            return null;
+        }
+
+        // 前序遍历中的第一个节点就是根节点
+        int preorder_root = preorder_left;
+        // 在中序遍历中定位根节点
+        int inorder_root = indexMap.get(preorder[preorder_root]);
+
+        // 先把根节点建立出来
+        TreeNode root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+
+    /**
+     * 验证回文字符串
+     * @param s
+     * @return
+     */
+    public static boolean isPalindrome(String s) {
+        s = s.replaceAll("[^A-Za-z0-9]", "");
+        if (s.trim().equals("")) {
+            return true;
+        }
+        char[] word = s.toLowerCase().toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        int i,j=0;
+        if (word.length % 2 != 0) {
+            i = word.length/2-1;
+            j=word.length/2 + 1;
+        }else {
+            i = word.length/2-1;
+            j=word.length/2;
+        }
+        for(  ;i >=0 ;i--, j++) {
+            if (word[i] != word[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 验证回文字符串，字符串逆转后比较是否相等
+     * @param s
+     * @return
+     */
+    public static boolean isPalindrome2(String s) {
+        StringBuffer sgood = new StringBuffer();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                sgood.append(Character.toLowerCase(ch));
+            }
+        }
+        StringBuffer sgood_rev = new StringBuffer(sgood).reverse();
+        return sgood.toString().equals(sgood_rev.toString());
+    }
+
+
+
 }
